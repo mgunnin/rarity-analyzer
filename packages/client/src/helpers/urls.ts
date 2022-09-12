97ed4bfcf4a3d947b23538e3e50e6eefe7e5b9d5
Matt Gunnin <matt@esports.one>
2022-09-12 12:26:14 -0500

    Changed IPFS replace url to Metagallery

--- packages/client/src/helpers/urls.ts
+++ packages/client/src/helpers/urls.ts
@@ -1,6 +1,9 @@
-export const getProjectsJsonUrl = ()=>`/data/projects.json`;
-export const getProjectJsonUrl = (projectKey:string)=>`/data/${projectKey}/project.json`;
-export const getNftJsonUrl = (projectKey:string, tokenId: string)=>`/data/${projectKey}/${tokenId}.json`;
-export const getIpfsUrl = (nftUrl:string) => nftUrl.replace('ipfs://', 'https://gateway.pinata.cloud/ipfs/');
-export const getOpenSeaTokenUrl = (contractAddress:string, tokenId:string) => `https://opensea.io/assets/${contractAddress}/${tokenId}`;
-
+export const getProjectsJsonUrl = () => `/data/projects.json`
+export const getProjectJsonUrl = (projectKey: string) =>
+  `/data/${projectKey}/project.json`
+export const getNftJsonUrl = (projectKey: string, tokenId: string) =>
+  `/data/${projectKey}/${tokenId}.json`
+export const getIpfsUrl = (nftUrl: string) =>
+  nftUrl.replace("ipfs://", "https://metagallery.com/ipfs/")
+export const getOpenSeaTokenUrl = (contractAddress: string, tokenId: string) =>
+  `https://opensea.io/assets/${contractAddress}/${tokenId}`
