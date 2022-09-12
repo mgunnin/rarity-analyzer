229919323ca15c607b4945e7e1a9fa96ba5553a8
Matt Gunnin <matt@esports.one>
2022-09-12 15:02:08 -0500

    More cleanup

--- packages/client/src/components/layout.tsx
+++ packages/client/src/components/layout.tsx
@@ -1,35 +1,40 @@
-import React, { useEffect, useState } from "react";
-import { ThemeSubscription } from "../helpers/theme";
-import { Icon } from "./icons";
+import React, { useEffect, useState } from "react"
+import { ThemeSubscription } from "../helpers/theme"
+import { Icon } from "./icons"
 
-export const Layout = ({ children }:{ children:JSX.Element }) => {
+export const Layout = ({ children }: { children: JSX.Element }) => {
+  const [isDark, setIsDark] = useState(true)
 
-    const [isDark, setIsDark] = useState(true);
+  useEffect(() => {
+    ThemeSubscription.subscribe(setIsDark)
+  }, [])
 
-    useEffect(()=>{
-        ThemeSubscription.subscribe(setIsDark);
-    },[]);
-
-    return (
-        <>
-            <div className='header' style={{display: 'flex', flexDirection:'row', padding: 8, alignItems:'flex-start'}}>
-                <div style={{fontSize: 36, marginRight: 16}}>
-                    <a href="/">
-                        OpenRarity
-                    {/* <img style={{width: 256}}
+  return (
+    <>
+      <div
+        className="header"
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          padding: 8,
+          alignItems: "flex-start",
+        }}
+      >
+        <div style={{ fontSize: 36, marginRight: 16 }}>
+          <a href="/">
+            OneDAO
+            {/* <img style={{width: 256}}
                         src={isDark ? '/media/logo_white.png' : '/media/logo_black.png'} alt='site-logo'/> */}
-                    </a>
-                </div>
-                <div style={{fontSize: 20}}>
-                    <a href="https://github.com/cryptoDevAmigos/rarity-analyzer">
-                        <Icon icon='github'/>
-                    </a>
-                </div>
-                <div style={{flex:1}}></div>
-            </div>
-            <div>
-                {children}
-            </div>
-        </>
-    );
-};
+          </a>
+        </div>
+        <div style={{ fontSize: 20 }}>
+          <a href="https://github.com/One-DAO/">
+            <Icon icon="github" />
+          </a>
+        </div>
+        <div style={{ flex: 1 }}></div>
+      </div>
+      <div>{children}</div>
+    </>
+  )
+}
